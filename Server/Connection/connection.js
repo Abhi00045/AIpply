0c9c2e17344024e9ace0d588e52d94e8d09a560b
Abhi00045 <rajidiabhinavreddy@gmail.com>
2025-06-06 01:21:20 +0530

    connected Db

--- Server/Connection/connection.js
+++ Server/Connection/connection.js
@@ -1,2 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const Connection = async ()=>{
+
+    try{
+        const db= process.env.MONGO_URI;
+        mongoose.connect(db);
+        console.log("DB Connected Successfully");
+        
+    }catch(err){
+        console.log(err);
+    }
+}
--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { Connection } from "./Connection/connection.js";
 
 const app = express();
 
+
 app.listen("2020" , (req , res)=>{
+    Connection();
     console.log("server Started");   
 })
