eaabc0c797257291649e9ed5fc0076ab9cd57c5d
Abhi00045 <rajidiabhinavreddy@gmail.com>
2025-06-14 02:29:32 +0530

    done with post validation on user

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,7 @@ import router from "./Routes/user.router.js";
 
 const app = express();
 
+app.use(express.json()) //this is the most important line inthe raw data posting in job portal
 app.use("/user", router);
 
 app.listen(3011 , (req , res)=>{
