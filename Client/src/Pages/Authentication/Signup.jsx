d11467d2e533b719a8d2374762570c83958c3eff
Abhi00045 <rajidiabhinavreddy@gmail.com>
2025-06-20 04:03:36 +0530

    added cors

--- Client/src/Pages/Authentication/Signup.jsx
+++ Client/src/Pages/Authentication/Signup.jsx
@@ -1,72 +1,96 @@
-import '../Authentication/Auth.css'
-import loginImage from '../../public/loginimage.png'
-import { useState } from 'react'
-import axios from 'axios'
+import "../Authentication/Auth.css";
+import loginImage from "../../public/loginimage.png";
+import { useState } from "react";
+import axios from "axios";
 
+export const Signup = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [role, setRole] = useState();
 
-export const Signup = ()=>{
-
-  const[name , setName] = useState();
-  const[email , setEmail] = useState();
-  const[password , setPassword] = useState();
-  const[role , setRole] = useState();
-
-  const submitingUsers = (e)=>{
+  const submitingUsers = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3011/user',{name , email , password , role})
-    .then(result => console.log(result)
-    ).catch(err => console.log(err));
-    }
+    // console.log(name, email, password, role);
+    axios
+      .post("http://localhost:3011/user", { fullName: name, email, password, role })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
 
+  return (
+    <>
+      <div className="signup-container">
+        <div className="signup-left">
+          <h1 className="title">Sign Up</h1>
+          <p className="subtitle">
+            Create your account to access the Job Portal Dashboard
+          </p>
 
-    return(
-        <>
-        <div className="signup-container">
-      <div className="signup-left">
-        <h1 className="title">Sign Up</h1>
-        <p className="subtitle">Create your account to access the Job Portal Dashboard</p>
+          <form className="signup-form" onSubmit={submitingUsers}>
+            <label htmlFor="">Name</label>
+            <input
+              id="inp"
+              type="text"
+              placeholder="Enter your Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label>Email</label>
+            <input
+              id="inp"
+              type="email"
+              placeholder="youremail@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-        <form className="signup-form" onSubmit={submitingUsers}>
-          <label htmlFor="">Name</label>
-          <input id='inp' type="text" 
-          placeholder='Enter your Name' 
-          onChange={(e)=>setName(e.target.value)} 
+            <label>Password</label>
+            <input
+              id="inp"
+              type="password"
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <label>Confirm Password</label>
+            <input
+              id="inp"
+              type="password"
+              placeholder="Re-enter your password"
+            />
+
+            <label htmlFor="Role">Who Are You?</label>
+            <select
+              className="role"
+              id="inp"
+              name="Who Are You ?"
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <option id="inp" value="jobseeker">
+                I am a jobseeker
+              </option>
+              <option id="inp" value="recuriter">
+                I am a recruiter
+              </option>
+            </select>
+
+            <button className="signup-btn" onClick={submitingUsers}>
+              Create Account
+            </button>
+
+            <p className="alternate">
+              Already have an account? <a href="/login">Log in</a>
+            </p>
+          </form>
+        </div>
+
+        <div className="signup-right">
+          <img
+            src={loginImage}
+            alt="Job Portal Illustration"
+            className="signup-illustration"
           />
-          <label>Email</label>
-          <input id='inp'
-           type="email" 
-           placeholder="youremail@example.com" 
-           onChange={(e)=>setEmail(e.target.value)}/>
-
-          <label>Password</label>
-          <input id='inp'
-           type="password" 
-           placeholder="Enter your password"
-           onChange={(e)=>setPassword(e.target.value)} />
-
-          <label>Confirm Password</label>
-          <input id='inp' type="password" placeholder="Re-enter your password" />
-          
-          <label htmlFor="Role">Who Are You?</label>
-          <select className='role' id="inp" name="Who Are You ?" onChange={(e)=>setRole(e.target.value)}>
-            <option id='inp' value="Applicant">I am a jobseeker</option>
-            <option id='inp' value="Recuriter">I am a recruiter</option>
-          </select>
-
-          <button className="signup-btn" onClick={submitingUsers} >Create Account</button>
-
-          <p className="alternate">Already have an account? <a href="/login">Log in</a></p>
-        </form>
+        </div>
       </div>
-
-      <div className="signup-right">
-        <img
-          src={loginImage}
-          alt="Job Portal Illustration"
-          className="signup-illustration"
-        />
-      </div>
-    </div>
-        </>
-    )
-}
+    </>
+  );
+};
--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,17 @@
 import express from "express";
+import cors from "cors";
 import { Connection } from "./Connection/connection.js";
 import router from "./Routes/user.router.js";
 
 const app = express();
-
-app.use(express.json()) //this is the most important line inthe raw data posting in job portal
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+};
+app.use(cors(corsOptions));
+app.use(express.json()); //this is the most important line inthe raw data posting in job portal
 app.use("/user", router);
 
-app.listen(3011 , (req , res)=>{
-    Connection();
-    console.log("server Started");
-})
+app.listen(3011, (req, res) => {
+  Connection();
+  console.log("server Started");
+});
