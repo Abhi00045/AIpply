0c21f9c76a29338bb35e70c68b49b55b4fd891f9
abhi <rajidiabhinavreddy@gmail.com>
2025-11-22 02:25:43 +0530

    fuck

--- Client/src/Pages/Applicant/FindJobs.jsx
+++ Client/src/Pages/Applicant/FindJobs.jsx
@@ -1,98 +1,90 @@
-
 import { IoMdNotifications } from "react-icons/io";
 import '../Applicant/Apply.css'
-import { HeaderCard } from "../../Components/Header-card";
-import { ResumeSocialCard } from "../../Components/Resume+Social-card";
-import { ProfileInfo } from "./Profile-info";
-import { useState, useEffect } from "react";
 import JobCards from '../../Components/JobsPost.jsx'
 import { Link, redirect } from "react-router";
+import { useState, useEffect } from "react";
 
+export const FindJobs = () => {
+  const [email, setEmail] = useState("");
 
-
-export const FindJobs =()=>{
-
-        const[email, setEmail]= useState('')
-
-    useEffect(()=>{
-
-    const userEmail = localStorage.getItem('user')
-    if(userEmail){
-      const user = JSON.parse(userEmail)
-      setEmail(user.email)
+  useEffect(() => {
+    const userEmail = localStorage.getItem("user");
+    if (userEmail) {
+      const user = JSON.parse(userEmail);
+      setEmail(user.email);
     }
-    },[])
-    const handleLogout = () => {
+  }, []);
+
+  const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
     redirect("/login");
   };
 
-    return(
+  return (
     <>
-            <div className="h-screen w-screen bg-black flex flex-row">
+      <div className="h-screen w-screen bg-black flex flex-row overflow-hidden">
 
-        {/* LEFT MENU BAR */}
-        <div className="w-[15%] p-3">
-          <div className="flex flex-col justify-between items-center h-full gap-12">
-
-            <Link className="text-white no-underline" to="/">
+        {/* LEFT SIDEBAR */}
+        <div className="w-[15%] p-5 bg-black text-white flex flex-col justify-between">
+          <div>
+            <Link to="/" className="no-underline">
               <h1 className="text-white text-3xl font-bold">AIpply</h1>
             </Link>
 
-            {/* MENU LIST */}
-            <div className="w-full">
-              <ul className="flex flex-col">
-                
-                <Link className="no-underline text-white" to="/applicant/findjobs">
-                  <li className="list-none w-[15vw] p-4 hover:bg-[#234f92]">Find Jobs</li>
-                </Link>
+            <ul className="mt-10 flex flex-col">
+              <Link to="/applicant/findjobs" className="no-underline text-white">
+                <li className="p-4 rounded-lg hover:bg-[#234f92] cursor-pointer">
+                  Find Jobs
+                </li>
+              </Link>
 
-                <Link className="no-underline text-white" to="/applicant/MockInterviews">
-                  <li className="list-none w-[15vw] p-4 hover:bg-[#234f92]">Mock Interviews</li>
-                </Link>
+              <Link to="/applicant/MockInterviews" className="no-underline text-white">
+                <li className="p-4 rounded-lg hover:bg-[#234f92] cursor-pointer">
+                  Mock Interviews
+                </li>
+              </Link>
 
-                <Link className="no-underline text-white" to="/applicant">
-                  <li className="list-none w-[15vw] p-4 hover:bg-[#234f92]">Profile</li>
-                </Link>
+              <Link to="/applicant" className="no-underline text-white">
+                <li className="p-4 rounded-lg hover:bg-[#234f92] cursor-pointer">
+                  Profile
+                </li>
+              </Link>
 
-                <Link className="no-underline text-white" to="/applicant/Applications">
-                  <li className="list-none w-[15vw] p-4 hover:bg-[#234f92]">Applications</li>
-                </Link>
-
-              </ul>
-            </div>
-
-            {/* LOGOUT BUTTON */}
-            <div>
-              <button
-                onClick={handleLogout}
-                className="w-[13vw] py-3 rounded-[25px] cursor-pointer border-2 border-[#234f92] bg-black text-white hover:text-red-500"
-              >
-                Log out
-              </button>
-            </div>
+              <Link to="/applicant/Applications" className="no-underline text-white">
+                <li className="p-4 rounded-lg hover:bg-[#234f92] cursor-pointer">
+                  Applications
+                </li>
+              </Link>
+            </ul>
           </div>
+
+          {/* LOGOUT */}
+          <button
+            onClick={handleLogout}
+            className="w-full py-3 rounded-[25px] border-2 border-[#234f92] bg-black text-white hover:text-red-500 transition"
+          >
+            Log out
+          </button>
         </div>
 
-        {/* CONTENT SECTION */}
-<div className="main-applicant-page">
-                <div className="main-page-navigation-bar">
-                <div className="menu-list">
-                    <h3>Jobs</h3>
-                </div>
-                <div className="menu-list">
-                {/* <IoMdNotifications color="white" /> */}
-                <pre>{email}</pre></div>
-                </div>
-                <hr />
-                <div className="profile-content">
-                    <JobCards/>
-        </div>
+        {/* MAIN CONTENT */}
+        <div className="flex-1 bg-white rounded-l-[25px] overflow-y-auto">
 
-            </div>
+          {/* NAVBAR */}
+          <div className="sticky top-0 bg-white z-10 shadow-md flex justify-between items-center px-6 py-5 rounded-t-[25px]">
+            <h3 className="text-black text-xl font-semibold">Jobs</h3>
+            <pre className="text-black">{email}</pre>
+          </div>
+
+          {/* CONTENT */}
+          <div className="p-6">
+            <JobCards />
+          </div>
+
+        </div>
 
       </div>
     </>
-    )
-}
+  );
+};
