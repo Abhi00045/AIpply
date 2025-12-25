3b00f1d7470a829b5f807147794e21444be4e378
abhi <rajidiabhinavreddy@gmail.com>
2025-12-25 23:28:44 +0530

    bitch

--- Client/src/Pages/Applicant/MockInterviews.jsx
+++ Client/src/Pages/Applicant/MockInterviews.jsx
@@ -2,21 +2,7 @@ import React from 'react'
 
 function MockInterviews() {
   return <>
-   <div className="main-applicant-page">
-                  <div className="main-page-navigation-bar">
-                  <div className="menu-list">
-                      <h3>Mock Interviews</h3>
-                  </div>
-                  <div className="menu-list">
-                  {/* <IoMdNotifications color="white" /> */}
-                  <pre>info@gmail.com</pre></div>
-                  </div>
-                  <hr />
-                  <div className="profile-content">
-                      {/* <ProfileInfo/> */}
-          </div>
-  
-              </div>
+  <h1>hiii</h1>
   </>
 }
 
