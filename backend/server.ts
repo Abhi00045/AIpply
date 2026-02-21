fa2ada7d9840310622e66922029bdd33c2ca4841
abhi <rajidiabhinavreddy@gmail.com>
2026-02-21 17:41:18 +0530

    not cool

--- backend/server.ts
+++
@@ -1,7 +0,0 @@
-import app from "./src/app"; // in type script no .ts should be ended
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
